c6ddd4ee393a208bf8bb669991e3ed400321149d
lijianing97 <cimu96991411@163.com>
2020-07-08 21:20:26 +0800

    Init Project

---
+++ heartstone/src/main/java/com/lijianing/hearthstone/HearthstoneApplication.java
@@ -0,0 +1,16 @@
+package com.lijianing.hearthstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lijianing
+ */
+@SpringBootApplication
+public class HearthstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HearthstoneApplication.class, args);
+    }
+
+}
