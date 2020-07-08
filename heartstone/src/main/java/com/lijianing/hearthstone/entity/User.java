0c548518721aea6489354a3f2e20f4c1d196c248
lijianing97 <cimu96991411@163.com>
2020-07-08 21:16:09 +0800

    Add entity User

---
+++ heartstone/src/main/java/com/lijianing/hearthstone/entity/User.java
@@ -0,0 +1,37 @@
+package com.lijianing.hearthstone.entity;
+
+/**
+ * @author lijianing
+ * @version 1.0
+ * @date 2020/7/8 21:13
+ */
+
+public class User {
+    private int id;
+    private String userName;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
